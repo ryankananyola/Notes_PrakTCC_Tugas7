85a099253f3e53b0c3f34fa4d6000f40b6707eab
ryankananyola <mariadestan2027@gmail.com>
2025-05-22 19:22:10 +0700

    update config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,14 @@
 import { Sequelize } from "sequelize";
+import "dotenv/config"
 
-const db = new Sequelize("notes-198", "root", "dbnotes198", {
-  host: "35.193.237.169",
+// Variabel yang menerima data rahasia
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+
+const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "mysql",
 });
 
