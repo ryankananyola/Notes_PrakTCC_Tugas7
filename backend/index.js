2e618922bbc54fa38045f2561975e76ba94c5c72
ryankananyola <mariadestan2027@gmail.com>
2025-05-31 08:10:18 +0700

    plislah ini plisss

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,10 @@ app.use(cookieParser());
 
 app.use(
   cors(
-    // {
-    //   origin: "http://localhost:3000",
-    //   credentials: true,
-    // }
+    {
+      origin: "http://localhost:3000",
+      credentials: true,
+    }
   )
 );
 app.use(express.json());
